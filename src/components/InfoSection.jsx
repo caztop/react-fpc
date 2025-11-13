f31c8e41991425783af8833e9f167212cdc54c3f
caztop <caztop@outlook.kr>
2025-11-13 22:23:15 +0900

    정보마당 파일 일부 수정

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -5,7 +5,7 @@ function InfoSection() {
     { img: 'apartments.png', label: '부동산 시장', url: 'info/property.png' },
     { img: 'tax.jpg', label: '세금', url: 'info/tax.png' },
     { img: 'tsave.png', label: '세제혜택 금융상품', url: 'info/tsave.png' },
-    { img: 'fsite.png', label: '재무설계관련 인터넷사이트', url: 'info/fsite.png' },
+    { img: 'fsite.png', label: '재무설계관련 인터넷사이트', url: 'info/fsite.pdf' },
   ];
 
   const handleClick = (url) => window.open(url, '_blank');
@@ -13,7 +13,7 @@ function InfoSection() {
   return (
     <>
       <div id="blank3"></div>
-      <div id="info">정보마당</div>
+      <div id="info">정보마당(조회의 편의성을 위해 일부는 화질저하가 있을 수 있음)</div>
       <div className="mbox">
         {boxes.map((box, i) => (
           <div className="box" key={i} onClick={() => handleClick(box.url)}>
