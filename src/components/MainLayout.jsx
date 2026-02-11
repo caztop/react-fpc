81a907d04317e076008070f45796a18e69abc95b
caztop <caztop@outlook.kr>
2026-02-11 17:18:46 +0900

    Refactor: RightSidebar 컴포넌트 분리 및 레이아웃 구조 개선

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,35 +1,13 @@
 import Sidebar from './Sidebar';
 import Slider from './Slider';
+import RightSidebar from './RightSidebar';
 
 function MainLayout() {
   return (
     <main className="main-layout">
       <Sidebar />
       <Slider />
-
-      <div className="right-sidebars">
-        <div className="right-sidebar" id="blank2"></div>
-
-        <div className="right-sidebar">
-          <p>- 재무설계와 금융상품(발간 도서) -</p>
-          <a
-            href="https://product.kyobobook.co.kr/detail/S000216848258"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/image/FinBookQR.png"
-              alt="재무설계와 금융상품 책구입 QR"
-              style={{ maxWidth: '100px', height: 'auto' }}
-            />
-          </a>
-          <br />
-          <span>QR을 스캔하거나 클릭</span>
-        </div>
-
-        <div className="right-sidebar">오픈 예정</div>
-        <div className="right-sidebar">오픈 예정</div>
-      </div>
+      <RightSidebar />
     </main>
   );
 }
