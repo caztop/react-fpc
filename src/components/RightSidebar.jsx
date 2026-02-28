03e3b6d7a74cc1ae812807763f06c6029be76613
caztop <caztop@outlook.kr>
2026-02-28 11:22:46 +0900

    right-sidebar에 blog 연결 및 css 개선

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -3,24 +3,25 @@ function RightSidebar() {
     <div className="right-sidebars">
       <div className="right-sidebar" id="blank2"></div>
 
-      <div className="right-sidebar">
+      <div className="right-sidebar qr-box">
         <p>- 재무설계와 금융상품(발간 도서) -</p>
-        <a
-          href="https://product.kyobobook.co.kr/detail/S000216848258"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="/image/FinBookQR.png"
-            alt="재무설계와 금융상품 책구입 QR"
-            style={{ maxWidth: '100px', height: 'auto' }}
-          />
-        </a>
-        <br />
-        <span>QR을 스캔하거나 클릭</span>
-      </div>
 
-      <div className="right-sidebar">FPC 공식블로그 곧 오픈</div>
+        <div className="qr-row">
+          <a
+            href="https://product.kyobobook.co.kr/detail/S000216848258"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="/image/FinBookQR.png" alt="QR" />
+          </a>
+          <span>QR을 스캔하거나 클릭</span>
+        </div>
+      </div>
+      <div className="right-sidebar">
+        <a href="https://blog.fpc-wp.com" target="_blank" rel="noopener noreferrer">
+        FPC 공식블로그<br />(상담사례,시장현황,투자방법 등 기록)
+        </a>
+      </div>
       <div className="right-sidebar">오픈 예정</div>
     </div>
   );
